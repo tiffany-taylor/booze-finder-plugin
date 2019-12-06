ad4c94714c7b59d0e44a70ff9c71f0ffb5781092
Tiffany <tiffany.k.taylor@gmail.com>
2019-12-06 13:51:01 -0600

    implement plugin endpoint

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,6 +2,20 @@
 
 namespace AsyncBot\Plugin\BoozeFinder;
 
+use Amp\Promise;
+use AsyncBot\Core\Http\Client;
+
 final class Plugin
 {
+    private Client $httpClient;
+
+    public function __construct(Client $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
+    public function search(string $keywords): Promise
+    {
+
+    }
 }
