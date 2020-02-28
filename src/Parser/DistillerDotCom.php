976766b932297e801d1e00704f9d23da63587116
Tiffany <tiffany.k.taylor@gmail.com>
2020-02-28 16:09:30 -0600

    update xpath query to use xpath_html_class helper function

--- src/Parser/DistillerDotCom.php
+++ src/Parser/DistillerDotCom.php
@@ -3,6 +3,8 @@
 
 namespace AsyncBot\Plugin\BoozeFinder\Parser;
 
+use function Room11\DOMUtils\xpath_html_class;
+
 final class DistillerDotCom
 {
     public function parse(\DOMDocument $dom)
@@ -16,6 +18,7 @@ final class DistillerDotCom
 
     private function isBoozeFound(\DOMXPath $xpath): bool
     {
-        return (bool) $xpath->evaluate("//ol[@class='spirits']/li[@class='spirit']")->length;
+        return (bool) $xpath->evaluate('//ol['.xpath_html_class('spirits').']/li['.xpath_html_class('spirit').']')->length;
     }
+
 }
